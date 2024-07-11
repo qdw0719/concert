a9d6af2fa9bc8b8cb0704c609fcb357b31696541
박영준\qdw07 <qdw0719@naver.com>
2024-07-12 02:26:29 +0900

    config, aspect, common, annotation 추가

---
+++ src/main/java/com/hb/concert/config/swagger/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.hb.concert.config.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("콘서트 예약 서비스 API")
+                        .version("1.0")
+                        .description("This is a sample Spring Boot RESTful service using Swagger"));
+    }
+}
