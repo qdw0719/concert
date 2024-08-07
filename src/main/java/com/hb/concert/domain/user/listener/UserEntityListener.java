98441ec1a9378fa3750dbbe6d26d0ceb9489e95b
박영준\qdw07 <qdw0719@naver.com>
2024-08-07 22:01:02 +0900

    transactional 누락 추가

--- src/main/java/com/hb/concert/domain/user/listener/UserEntityListener.java
+++ src/main/java/com/hb/concert/domain/user/listener/UserEntityListener.java
@@ -6,11 +6,12 @@ import com.hb.concert.domain.user.UserHistory;
 import com.hb.concert.domain.user.repository.UserRepository;
 import jakarta.persistence.PostPersist;
 import jakarta.persistence.PostUpdate;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
 public class UserEntityListener {
-    @PostPersist @PostUpdate
+    @PostPersist @PostUpdate @Transactional
     public void persistAndUpdate(User user) {
         UserRepository userRepository = BeanUtil.getBean(UserRepository.class);
 
