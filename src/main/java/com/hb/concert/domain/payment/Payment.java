d15ade8a154e00de70a289cd278f9d16ab1709c8
박영준\qdw07 <qdw0719@naver.com>
2024-08-08 22:08:04 +0900

    index추가, readme 추가

---
+++ src/main/java/com/hb/concert/domain/payment/Payment.java
@@ -0,0 +1,31 @@
+package com.hb.concert.domain.payment;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor @AllArgsConstructor
+@Builder @Data
+@Entity @Table(name = "HB_PAYMENT"
+        , indexes = { @Index(name = "idx_payment", columnList = "reservationId") }
+)
+public class Payment {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String reservationId;
+    private LocalDateTime effectiveTime;
+    private LocalDateTime paidTime;
+
+    public void createPayment(String reservationId) {
+        this.reservationId = reservationId;
+        this.effectiveTime = LocalDateTime.now().plusMinutes(5);
+    }
+
+    public void successPayment() {
+        paidTime = LocalDateTime.now();
+    }
+}
